d6d5c1764394cb4858e895d1878d6e8dac9e7f92
Jiaxin Liao <361946665@qq.com>
2026-02-13 18:55:49 +0800

    实现注册登录界面

--- frontend/src/views/user/account/LoginIndex.vue
+++ frontend/src/views/user/account/LoginIndex.vue
@@ -1,5 +1,23 @@
 <template>
- 登录
+<div class = "flex justify-center mt-30">
+  <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+  <legend class="fieldset-legend">Login</legend>
+
+  <label class="label">用户名</label>
+  <input type="email" class="input" placeholder="Email" />
+
+  <label class="label">密码</label>
+  <input type="password" class="input" placeholder="Password" />
+
+  <button class="btn btn-neutral mt-4">登录</button>
+  <div class = "flex justify-end">
+    <router-link :to = "{ name : 'user-account-register-index'}" class = "btn btn-sm btn-ghost text-gray-500" >注册</router-link>
+
+  </div>
+  </fieldset>
+</div>
+
+
 </template>
 
 <script>
--- frontend/src/views/user/account/RegisterIndex.vue
+++ frontend/src/views/user/account/RegisterIndex.vue
@@ -1,11 +1,33 @@
 <template>
- 
+<div class = "flex justify-center mt-30">
+  <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+  <legend class="fieldset-legend">Login</legend>
+
+  <label class="label">输入用户名</label>
+  <input type="email" class="input" placeholder="Email" />
+
+  <label class="label">输入密码</label>
+  <input type="password" class="input" placeholder="Password" />
+
+    <label class="label">确认密码</label>
+  <input type="password" class="input" placeholder="Password" />
+  <button class="btn btn-neutral mt-4">注册</button>
+  <div class = "flex justify-end">
+    <router-link :to = "{ name : 'user-account-login-index'}" class = "btn btn-sm btn-ghost text-gray-500" >
+      登录
+    </router-link>
+
+  </div>
+  </fieldset>
+</div>
+
+
 </template>
 
 <script>
 
 </script>
 
-<style scoped>
+<style >
 
 </style>

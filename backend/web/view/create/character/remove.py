1a92fb597babbd472f60abd4af91efc10296a720
neoHome <361946665@qq.com>
2026-02-23 21:09:56 +0800

    创建虚拟角色补丁

--- backend/web/view/create/character/remove.py
+++ backend/web/view/create/character/remove.py
@@ -5,12 +5,19 @@ from rest_framework.permissions import IsAuthenticated
 from web.models.character import Character
 from web.models.user import UserProfile
 
+from web.view.utils.photo import remove_old_photo
+
+#删除时，头像和背景一并删掉
 class RemoveCharacterView(APIView):
     permission_classes = [IsAuthenticated]
     def post(self,request):
         try:
             character_id = request.data['character_id']
-            Character.objects.filter(pk = character_id, author__user = request.user).delete()
+            #get 和filer 区别，前者返回一个元素，后者返回数组
+            character = Character.objects.get(pk = character_id, author__user = request.user)
+            remove_old_photo(character.photo)
+            remove_old_photo(character.background_image)
+            character.delete()
             return Response({
                 'result':'success',
             })
